f991535ba594a52fb329de78d5ee5ab461eeb884
LaraS <lara.stas@ugent.be>
2013-11-28 17:23:04 +0100

    Update attanx3-data.R

--- R/attanx3-data.R
+++ R/attanx3-data.R
@@ -37,7 +37,7 @@ The data are presented in the long format.
 }
 \usage{data(attanx3)}
 \format{
-  A data frame with 1249 variables (comming from 208 different families) containing four different variables. 
+  A data frame with 1249 variables (coming from 208 different families) containing four different variables. 
   \describe{
     \item{\code{family.id}}{An indicator for the family.}
     \item{\code{perceiver.id}}{An indicator of the rater in the dyad, either "m", "f", or "y"}
@@ -47,7 +47,7 @@ The data are presented in the long format.
     }
   }
 \source{
-This dataset was retrieved from \url{http://davidakenny.net/kkc/c9/c9.htm} and converted to an R dataset.
+This dataset was retrieved from \url{http://davidakenny.net/kkc/c9/c9.htm} in wide format and converted to an R dataset in long format.
 }    
 \references{
 Collins, N. L., & Read, S. J. (1990, April). Adult attachment, working models, and
