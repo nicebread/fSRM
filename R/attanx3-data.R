75542701747ec73afea4556187106ff7a6e8f847
LaraS <lara.stas@ugent.be>
2013-11-28 15:58:29 +0100

    Update attanx3-data.R
    Description attanx3 dataset

--- R/attanx3-data.R
+++ R/attanx3-data.R
@@ -19,4 +19,42 @@
 #' @keywords datasets
 #' @format A data frame with 1248 rows and 5 variables (208 families with 3 members each, round-robin design)
 #' @name attanx3
-NULL
+
+\name{attanx3}
+\alias{attanx3}
+\docType{data}
+\title{
+Attachment anxiety in three person families
+}
+\description{
+The classic Cook (2000) dataset consists of measurements on security of attachment within families. 
+Only the variable measuring fear of rejection in family relationships is included in this dataset. 
+Cook (2000) deduced this variable from the anxiety dimension of the adult attachment scale (Collins & Read, 1990).
+The orignial data consisted of four person families (i.e. two parents and two children), but in order to obtain 
+a three person family the oldest sibling is systematically deleted in accordance with Kenny, Kashy & Cook (2006). 
+The data are presented in the long format.
+}
+\usage{data(attanx3)}
+\format{
+  A data frame with 1249 variables (comming from 208 different families) containing four different variables. 
+  \describe{
+    \item{\code{family.id}}{An indicator for the family.}
+    \item{\code{perceiver.id}}{An indicator of the rater in the dyad, either "m", "f", or "y"}
+    \item{\code{target.id}}{Identifier of the person being rated in the dyad, either "m", "f", or "y"}
+    \item{\code{anx1}}{First indicator of relationship specific anxiety (i.e. average of first halve of the scale)}
+    \item{\code{anx1}}{Second indicator of relationship specific anxiety (i.e. average of second halve of the scale)}
+    }
+  }
+\source{
+This dataset was retrieved from \url{http://davidakenny.net/kkc/c9/c9.htm} and converted to an R dataset.
+}    
+\references{
+Cook, W. L. (2000). Understanding attachment security in family context. Journal of
+Personality and Social Psychology, 78 (2), 285–294.
+
+Kenny, D. A., Kashy, D. A., & Cook, W. L. (2006). Dyadic Data Analysis. United States of America: The Guilford Press.
+}
+
+\examples{
+head(attanx3)
+}
