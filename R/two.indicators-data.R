b5daf807ac9dc48e949bd5c40baddf61be141ba3
LaraS <lara.stas@ugent.be>
2014-01-31 15:16:05 +0100

    Update two.indicators-data.R

--- R/two.indicators-data.R
+++ R/two.indicators-data.R
@@ -1,16 +1,19 @@
 #' Data set on attachment dependency (Cook, 2000)
 #' 
-#' A dataset containing ... TODO.
-#' Four roles are present: Mothers "m", fathers "f", the older child "c", and the younger child "y".
+#' The classic Cook (2000) dataset consists of measurements on
+#' security of attachment within families. Only the variable
+#' measuring attachment dependency in family relationships is
+#' included in this dataset. Four roles are present (i.e. two parents and two children):
+#' mothers "m", fathers "f", the older
+#' child as "c", and the younger child "y".
 #'
 #' The variables are as follows:
 #' 
 #' \itemize{
 #'   \item family.id An indicator for the family.
-#'   \item actor.id An indicator for the perceiver, either "m", "f", "c", or "y"
-#'   \item partner.id An indicator for the target, either "m", "f", "c", or "y"
-#'   \item dep1 TODO: Describe dependency measure.
-#'   \item dep2 TODO: Describe dependency measure.
+#'   \item actor.id An indicator for the perceiver, either "m", "f", "c", or "y". 
+#'   \item partner.id An indicator for the target, either "m", "f", "c", or "y". 
+#'   \item dep1 first measurement of attachment dependency. 
 #' }
 #' 
 #' @references Cook, W. L. (2000). Understanding attachment security in family context. \emph{Journal of Personality and Social Psychology, 78}, 285–294. doi:10.1037/0022-3514.78.2.285
