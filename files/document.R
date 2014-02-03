4e06e317c2d06bf363f6dfb601399de57527efa2
Felix Schönbrodt <felix@nicebread.de>
2014-02-03 14:39:26 +0100

    Fix for new structure

--- files/document.R
+++ files/document.R
@@ -1,5 +1,5 @@
 # check for non-ASCII characters
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 library(tools)
 for (nm in list.files("R/.", pattern="\\.[r|R]", recursive=FALSE)) {
 	cat(nm,":")    
@@ -9,21 +9,21 @@ for (nm in list.files("R/.", pattern="\\.[r|R]", recursive=FALSE)) {
 
 library(devtools)
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::load_code()
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::document()
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::check()
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::install()
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::build_win()
 
-setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM")
+setwd("/Users/Felix/Documents/R/Funktionen/GitHub/fSRM/package")
 devtools::release()
 
