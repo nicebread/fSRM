86269682157fe1f818c066cc8a4812aa58d814f2
LaraS <lara.stas@ugent.be>
2014-09-19 20:45:48 +0200

    Update clinical-data.R

--- package/R/clinical-data.R
+++ package/R/clinical-data.R
@@ -2,10 +2,16 @@
 
 #' A simulated dataset, mimicking the study performed by Eichelsheim et al. (2011) who investigated whether there are
 #' differences in patterns of negativity between families with and without an adolescent with externalizing problem
-#' behavior. The problematic and nonproblematic group consist of 120 and 153 four-person families, respectively.
-#' This dataset contains a measures of negativity for each of the 12 relationships.
+#' behavior. In the study of Eichelsheim et al. (2011), four members of the same family (mother, father, target 
+#' adolescent and sibling) reported on the amount of negativity they experienced in relation to each other. 
+#' In total, these authors studied 120 Dutch four-person families with a target adolescent scoring above the 
+#' externalizing behavior clinical norm scores on either the Child Behavior Check List (N = 47; CBCL; Achenbach, 1991) 
+#' or the Youth Self Report (N = 73; YSR; Achenbach, 1991). Because of confidentiality reasons, not the original data 
+#' but mimicked data are used here, so results of social relations analyses will deviate from the original paper. 
+#' 
+#' In sum, this dataset contains a measures of negativity for each of the 12 relationships.
 #' Four roles are present: Mothers "M", fathers "F", the asolescent with externalizing problem behavior "T", and the adolescent sibling without problem behavior "S".
-#' A wide version of the same data set is in \code{two.groups.wide}.
+#' A wide version of the same data set is in \code{clinical.wide}.
 #'
 #' The variables are as follows:
 #'
